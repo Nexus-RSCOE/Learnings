540ad878120cd9ff9000ab067fb8dc7a99e51075
Soham <rimatip472@operades.com>
2024-12-02 19:44:19 +0530

    Day 1 task completed!

---
+++ 30-days-js/Soham Kulkarni/day01.js
@@ -0,0 +1,12 @@
+let firstName = 'soham'
+let lastName = 'kulkarni' 
+let country = 'india' 
+let city = 'pune' 
+let age = 19 
+let isCommitted= true //i wish xd
+ 
+const PI = 3.14 
+
+let name = 'soham', 
+  branch = 'IT',
+  live = 'pune'
