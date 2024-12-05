8016ac8dc1681fce63cba8fa06757f49203b1425
Soham <rimatip472@operades.com>
2024-12-05 22:32:39 +0530

    day03 code

---
+++ 30-days-js/Soham Kulkarni/day03.js
@@ -0,0 +1,19 @@
+// non-primitive data types (object, array, and function)
+//object
+let person = {
+    name: "Soham",
+    age: 19,
+    isStudent: true,
+};
+//array 
+let colors = ["red", "green", "blue"];
+
+console.log(person.name);    
+console.log(person.age);     
+console.log(colors[0]);      
+console.log(colors[1]);    
+
+function displayInfo() {
+    console.log("This is a function in JS.");
+}
+displayInfo();  
